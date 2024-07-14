2a283c75544d3fe3a6866b37b3cb61641a73a673
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2024-07-14 17:54:59 +0900

    ユーザーログイン機能

--- src/repositories/auth.js
+++ src/repositories/auth.js
@@ -7,6 +7,10 @@ export const authRepository = {
         })
         if (error!= null) throw new Error(error.message);
         return {...data.user,userName:data.user.user_metadata.name}
-        
+    },
+    async signin (email,password){
+        const {data,error} = await supabase.auth.signInWithPassword({email,password})
+        if (error) throw new Error(error.message)
+        return{...data.user,userName: data.user.user_metadata.name}
     }
 }

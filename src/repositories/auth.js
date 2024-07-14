6ca2c6c2b09a76e9bdeba7f28d8145c98b315585
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2024-07-14 18:30:21 +0900

    JWT

--- src/repositories/auth.js
+++ src/repositories/auth.js
@@ -12,5 +12,14 @@ export const authRepository = {
         const {data,error} = await supabase.auth.signInWithPassword({email,password})
         if (error) throw new Error(error.message)
         return{...data.user,userName: data.user.user_metadata.name}
+    },
+    async getCurrentUser(){
+        const {data,error} = await supabase.auth.getSession();
+        if (error != null) throw new Error(error.message)
+        if (data.session == null) return;
+        return {
+            ...data.session.user,
+            userName: data.session.user.user_metadata.name
+        }
     }
 }

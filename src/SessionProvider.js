c7ecbde0526867173b138a4b1d5d24df1bb8b898
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2024-07-14 18:15:03 +0900

    グローバルステートでログイン保持

---
+++ src/SessionProvider.js
@@ -0,0 +1,10 @@
+import { createContext, useState } from "react";
+
+const SessionContext = createContext()
+const SessionProvider = (props)=>{
+    const [currentUser,setCurrentUser] = useState()
+    return <SessionContext.Provider value={{currentUser,setCurrentUser}}>
+    {props.children}
+    </SessionContext.Provider>
+}
+export {SessionContext,SessionProvider}
